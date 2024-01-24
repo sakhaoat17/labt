6baf8602d15d1d197113b39b951f0e46c5ba4344
tanjim <tanjimhasan35@gmail.com>
2024-01-24 09:37:22 +0600

    all file added

--- test.java
+++ test.java
@@ -1,32 +1,4 @@
-public interface Shape {
-    void draw();
-    }
-    public class Rectangle implements Shape {
-    @Override
-    public void draw() {
-    System.out.println("Inside Rectangle::draw() method.");
-    }
-    }
-    public class Square implements Shape {
-    @Override
-    public void draw() {
-    System.out.println("Inside Square::draw() method.");
-    }
-    }
-    public class ShapeFactory {
-    public Shape getShape(String shapeType){
-    if(shapeType == null){
-    return null;
-    }
-     if(shapeType.equalsIgnoreCase("RECTANGLE")){
-    return new Rectangle();
-    } else if(shapeType.equalsIgnoreCase("SQUARE")){
-    return new Square();
-    }
-    return null;
-    }
-    }
-    public class FactoryPatternDemo {
+ public class test {
     public static void main(String[] args) {
     ShapeFactory shapeFactory = new ShapeFactory();
     Shape shape1 = shapeFactory.getShape("CIRCLE");shape1.draw();
